fb7655fea5bd4ed74c9f5314bad01e8cbd030e64
Tmoney$ <15959707+tpmiller87@users.noreply.github.com>
2023-03-06 08:33:09 -0500

    Update ease.sh

--- ease.sh
+++ ease.sh
@@ -2,12 +2,18 @@
 
 file=$2
 
+#All lists should be pulled from the appropriate outpute file
+#For CME, this is in the ~/.cme/logs directory after running an ntds dump
+#For MSF, this is wherever you decided to put the output file in the MSF options
 
 #########FUNCTIONS START#########
 
 Help () {
 #Display Help
 #option h
+	echo "All lists should be pulled from the appropriate outpute file"
+	echo "For CME, this is in the ~/.cme/logs directory after running an ntds dump"
+	echo -e "For MSF, this is wherever you decided to put the output file in the MSF options" '\n'
 	echo "Syntax: ./ease.sh [-h|n|c|l|a] <filename>"
 	echo "options:"
 	echo "-h     Print help!"
@@ -19,27 +25,27 @@ Help () {
 }
 
 ntlm () {
-#Tool format: CME
+#Tool format: CME,MSF
 #Removes machine accounts and gives you the only the NTLM hash.
 #Should be used for cracking
 #option n (for ntlm)
-	grep -v "$+*" $file| awk '{print $5}' | grep -v -e 'cbc-md5' -e 'hmac-sha1' | awk -F ':' '/:/ {print $4}'
+	grep -v "$+*" $file | grep -v -e 'cbc-md5' -e 'hmac-sha1' | cut -d ':' -f 4
 }
 
 hashuser () {
-#Tool format: CME
+#Tool format: CME,MSF
 #Removes machine accounts and gives you 'hash:user'
 #Should be used for correlating cracked hashes from hashtopolis.
 #option c (for correlate, lame)
-	grep -v "$+*" $file | awk '{print $5}' | grep -v -e 'cbc-md5' -e 'hmac-sha1' | cut -d '\' -f 2|  awk -F ':' '/:/ {print $4":"$1}'
+	grep -v "$+*" raze.creds.ntds | grep -v -e 'cbc-md5' -e 'hmac-sha1' | cut -d '\' -f 2|  awk -F ':' '/:/ {print $4":"$1}'
 }
 
 userlist () {
-#Tool format: CME
+#Tool format: CME,MSF
 #Removes machine accounts and only username without domain attached.
 #Should be used for making user lists
 #option l (for list!)
-	grep -v "$+*" $file | awk '{print $5}' | grep -v -e 'cbc-md5' -e 'hmac-sha1' | awk -F ':' '/:/ {print $1}' | cut -d '\' -f 2
+	grep -v "$+*" $file | grep -v -e 'cbc-md5' -e 'hmac-sha1' | cut -d '\' -f 2 | cut -d ':' -f 1
 }
 
 all () {
